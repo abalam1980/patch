86b4ff83619cb2b1a6f64e1ee0348d35c4e9f04d
cjh1249131356 <cjh1249131356@gmail.com>
2021-02-17 18:52:23 +0800

    fix Hasty kernel build

--- patch.sh
+++ patch.sh
@@ -8,5 +8,8 @@ cd $ROOTDIR/frameworks/base
 git am $CURRENT_DIR/padding.patch
 git am $CURRENT_DIR/headsup.patch
 
+cd $ROOTDIR/kernel/oneplus/sm8250
+git am $CURRENT_DIR/kernel.patch
+
 cd $ROOTDIR
 echo "Done"
